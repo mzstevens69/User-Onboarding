6cc1c361f573f27e36d3319d2796445f9d89369c
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-11-14 15:46:41 -0600

    have basic layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import { Form, Field, withFormik  } from 'formik'
+import { Form, Field, withFormik, yupToFormErrors  } from 'formik'
 import *as Yup from 'yup';
 
 const UserForm = ({ errors, touched, values, status }) => {
@@ -25,11 +25,48 @@ const UserForm = ({ errors, touched, values, status }) => {
                 <Field type="text" name="email" placeholder="email" />
                 {touched.email && errors.email && (
                 <p className="error">{errors.email}</p>
-        )}
-
+                )}
+                <label 
+                className='checkbox-container'>
+                Terms of Service
+                <Field 
+                type='checkbox'
+                name='tos'
+                checked={values.tos}
+                />
+                <span className='checkmark'/>
+                </label>
+                <button type="submit">Submit!</button>
             </Form>
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>Users: {user.name} </li>
+                    <li>Users: {user.email} </li>
+                    <li>Users: {user.password} </li>
+                </ul>
+            ))}
         </div>
-    )
-}
+    );
+};
+const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, tos}) {
+        return {
+            name: name || '',
+            email: email || ''
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required(),
+        email: Yup.string().required()
+    }),
+    handleSubmit(values, {setStatus}) {
+        axios.post('https://reqres.in/api/users', values)
+        .then(res => {
+            setStatus(res.data);            
+        })
+        .catch(err => console.log(err.res)
+        );
+    }
+}) (UserForm);
 
-export default Form;
+export default FormikUserForm;
